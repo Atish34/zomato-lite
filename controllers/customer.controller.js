9ef49d088edbbb329c54bd21bc765f996a6de5b9
Your Name <you@example.com>
2025-01-11 10:55:28 +0530

    update customer controller

---
+++ controllers/customer.controller.js
@@ -0,0 +1,46 @@
+const asyncHandler = require("express-async-handler")
+const axios = require('axios')
+const { checkEmpty } = require("../utils/checkEmpty")
+const Customer = require("../models/Customer")
+
+exports.getLocation = asyncHandler(async(req,res) => {
+    const {latitude,longitude} = req.body
+    const {isError,error} = checkEmpty({latitude,longitude})
+
+    if(isError){
+        return res.status(400)
+        .json({message:"all felds required",error})
+    }
+    const {data} = await axios.get(`https://api.opencagedata.com/geocode/v1/json?q=${latitude}%2C${longitude}&key=${process.env.CAGE_API_KEY}`)
+    
+    let str = ""
+    let city = data.result[0].components.city
+    str += "" + data.results[0].components.road
+    str += "" + data.results[0].components.neighbourhood
+    str += "" + data.results[0].components.suburb
+    str += "" + data.results[0].components.city
+    str += "" + data.results[0].components.postcode
+
+
+    res.json({message:"location fetch success",result:str,city})
+    //setLoc(data.results[0].formatted)
+})
+
+exports.updateCustomerInfo = asyncHandler(async(req,res) => {
+    const {address,city,gender} = req.body
+    const {isError,error} = checkEmpty({address,city,gender})
+
+    if(isError){
+        return res.status(400)
+        res.json({message:"all felds required",error})
+    }
+   
+    const {result} = await Customer.findByIdAndUpdate(req.user,{
+        address,
+        city,
+        gender,
+        infoComplete:true,
+    },{new:true})
+    res.json({message:"profile update success",result})
+    //setLoc(data.results[0].formatted)
+})
