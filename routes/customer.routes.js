e36eb589d0dd4b6d53899e1929839ebe5d46f280
Your Name <you@example.com>
2025-01-14 10:57:38 +0530

    rechange in resturant controller

--- routes/customer.routes.js
+++ routes/customer.routes.js
@@ -6,6 +6,6 @@ const router = require("express").Router()
 router
     .post("/get-location", getLocation)
     .post("/update-info", updateCustomerInfo)
-    .post("/get-resturant", getResturant)
+    .get("/get-resturant", getResturant)
     
 module.exports = router
