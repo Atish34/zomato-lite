3315d97a240d6e853d8d87e6098ee15e447ed8e7
Your Name <you@example.com>
2025-01-23 10:27:33 +0530

    customer mobile modify

--- controllers/customer.controller.js
+++ controllers/customer.controller.js
@@ -71,6 +71,7 @@ exports.placedOrder = asyncHandler(async (req,res)=>{
 exports.getOrder = asyncHandler(async (req,res)=>{
     const result = await Order
     .find({customer:req.user}).select("-customer -createdAt -updatedAt -__v")
+    .populate("rider","name mobile")
     .populate("resturant","name hero")
     .populate("items.dish","name type image price")
     .sort({createdAt: -1})
